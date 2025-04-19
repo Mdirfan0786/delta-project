0748ff46f3da8aaef2f37a4bf3cd8da461eaeb5d
Md Irfan <rn555666777@gmail.com>
2025-04-19 21:37:32 +0530

    Add Project Files

--- controllers/filters.js
+++ controllers/filters.js
@@ -1,12 +1,12 @@
 const Listing = require("../models/listing.js");
 
 module.exports.filters = async (req, res) => {
-  const filters = await Listing.find({ category });
+  const filters = await Listing.find({ category: "trending" });
   if (!filters.length) {
-    req.flash("error", "filters for this listing not found!");
+    req.flash("error", "No listings found for this category!");
     res.redirect("/listings");
   } else {
-    req.flash("success", "Here is some listing for this filter!");
+    req.flash("success", "Here are some listings for this category!");
   }
   res.render("filters/filters.ejs", { filters });
 };
